5e3d8cfda26938893f50dc36f30521f265ef21d2
Jinming.Yang <jinming.yang@qingteng.cn>
2023-10-17 11:01:21 +0800

    feat: 数据处理逻辑修改，翻译文件更新

--- utils/loader.py
+++ utils/loader.py
@@ -12,7 +12,6 @@ import os
 from tqdm import tqdm
 
 from constants import Constants
-from translation.baidu import translate
 from utils import logger
 from .classes import IPSource
 
@@ -46,37 +45,43 @@ def load_data():
             ip_data.append(IPSource.from_data(*row))
         logger.info(f'数据加载完成，总数据量：{len(ip_data)}')
     # 防止单次请求因意外原因中断，可以接续完成后续数据的写入（翻译接口是有限额的）
+    translated_data = []
     try:
-        with open(Constants.PATH_TARGET, 'w+', encoding='utf-8') as f:
-            start_index = len(f.readlines())
-            end_index = len(ip_data) - 1
-            logger.info(f'开始数据转换')
-            translated_data = []
-            for index in tqdm(range(start_index, end_index)):
-                item: IPSource = ip_data[index]
-                tmp = [str(item.start), str(item.end), item.country_code]
-                # 国家信息
-                if item.country not in country_translate_dict:
-                    country_translate_dict[item.country] = translate(item.country)
+        start_index = 0
+        end_index = len(ip_data) - 1
+        logger.info(f'开始数据转换')
+        for index in tqdm(range(start_index, end_index)):
+            item: IPSource = ip_data[index]
+            tmp = [str(item.start), str(item.end)]
+            # 国家信息
+            if item.country_code in ('CN', 'HK', 'MO', 'TW'):
+                tmp.append('CN')
+                tmp.append('中国')
+            else:
+                tmp.append(item.country_code)
                 tmp.append(country_translate_dict.get(item.country, item.country))
-                # 区域信息
-                if item.region not in region_translate_dict:
-                    region_translate_dict[item.region] = translate(item.region)
+            # 区域信息
+            if item.country_code == 'TW':
+                tmp.append('台湾')
+            else:
                 tmp.append(region_translate_dict.get(item.region, item.region))
-                # 城市信息
-                if item.city not in city_translate_dict:
-                    city_translate_dict[item.city] = translate(item.city)
+            # 城市信息
+            if item.country_code == 'HK':
+                # 香港数据精细到了街道，这里直接统一香港
+                tmp.append('香港')
+            else:
                 tmp.append(city_translate_dict.get(item.city, item.city))
-                # 经纬度
-                # tmp.append(row.latitude)
-                # tmp.append(row.longitude)
-                translated_data.append('|'.join(tmp))
-            logger.info(f'数据转换完成')
+            # 经纬度
+            # tmp.append(row.latitude)
+            # tmp.append(row.longitude)
+            translated_data.append('|'.join(tmp) + '\n')
+        logger.info(f'数据转换完成')
     except Exception as ex:
         logger.exception(ex)
-        logger.info(f'当前: {start_index=}')
     finally:
         # 避免每次变更都写数据拖慢处理进度
-        json.dump(country_translate_dict, open(Constants.PATH_COUNTRY, 'w'), indent=4, ensure_ascii=False)
-        json.dump(region_translate_dict, open(Constants.PATH_REGION, 'w'), indent=4, ensure_ascii=False)
-        json.dump(city_translate_dict, open(Constants.PATH_CITY, 'w'), indent=4, ensure_ascii=False)
+        with open('./ip_data.txt', 'w', encoding='utf-8') as f:
+            f.writelines(translated_data)
+        # json.dump(country_translate_dict, open(Constants.PATH_COUNTRY, 'w'), indent=4, ensure_ascii=False)
+        # json.dump(region_translate_dict, open(Constants.PATH_REGION, 'w'), indent=4, ensure_ascii=False)
+        # json.dump(city_translate_dict, open(Constants.PATH_CITY, 'w'), indent=4, ensure_ascii=False)
